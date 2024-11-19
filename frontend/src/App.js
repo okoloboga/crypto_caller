6074ad13247ea09da98d6ae38457d0f8bca117c1
user <okoloboga37@gamil.com>
2024-11-19 20:22:30 +0300

    fixing reading manifest.json

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { TonConnectUIProvider, useTonAddress } from '@tonconnect/ui-react';
+import { TonConnectUIProvider, useTonAddress, TonConnectButton } from '@tonconnect/ui-react';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 
 const App = () => {
-  const walletAddress = useTonAddress(); // Получаем адрес кошелька
+  //const walletAddress = useTonAddress(); // Получаем адрес кошелька
+  //console.log(`Wallet address: ${walletAddress}`);
 
   return (
     <TonConnectUIProvider manifestUrl="https://caller.ruble.website/manifest.json">
       <div>
-        <Login /> {/* Показываем Login, если адреса нет */}
-        {walletAddress && <Dashboard walletAddress={walletAddress} />} {/* Показываем Dashboard, если адрес есть */}
+        <TonConnectButton />
+        {/* <Dashboard walletAddress={walletAddress} />  */}
       </div>
     </TonConnectUIProvider>
   );
