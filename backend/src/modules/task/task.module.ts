53ad724f6b1b82d84a4f28880e433297854a5a76
user <okoloboga37@gamil.com>
2024-11-28 10:14:05 +0300

    fixing getting tasks from DB

--- backend/src/modules/task/task.module.ts
+++ backend/src/modules/task/task.module.ts
@@ -4,16 +4,19 @@ import { Task } from './task.entity';
 import { TaskService } from './task.service';
 import { TaskController } from './task.controller';
 import { SharedModule } from '../../shared/shared.module';
-import { NotificationModule } from '../notification/notification.module';  // Импортируем NotificationModule
+import { NotificationModule } from '../notification/notification.module';
+import { UserModule } from '../user/user.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Task]),
     SharedModule,
-    forwardRef(() => NotificationModule),  // Используем forwardRef для отсроченного импорта
+    forwardRef(() => NotificationModule),
+    UserModule, 
   ],
   providers: [TaskService],
   controllers: [TaskController],
   exports: [TaskService],
 })
 export class TaskModule {}
+
--- backend/src/modules/user/user.module.ts
+++ backend/src/modules/user/user.module.ts
@@ -8,6 +8,6 @@ import { User } from './user.entity';
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
-  exports: [UserService],
+  exports: [TypeOrmModule.forFeature([User])],
 })
 export class UserModule {}
