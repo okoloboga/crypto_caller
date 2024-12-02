f1db078117b846dbbbb3e295faf1f00d5097a0ee
user <okoloboga37@gamil.com>
2024-12-02 18:51:51 +0300

    after hard reset...

--- src/shared/constants.ts
+++
@@ -1,4 +0,0 @@
-export const MAX_POINTS = 100;
-export const POINTS_INTERVAL = 3600; // 1 hour in seconds
-export const PRICE_CHECK_INTERVAL = 30; // 30 seconds for price monitoring
-export const POINTS_COLLECTION_INTERVAL = 21600; // 6 hours in seconds
