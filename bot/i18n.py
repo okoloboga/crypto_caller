1ffba31fbf152b716cb4e537b4ddb68a0ceaa428
okoloboga <okoloboga37@gmail.com>
2025-03-16 15:37:56 +0300

    fixing middleware bot

--- bot/i18n.py
+++ bot/i18n.py
@@ -1,6 +1,26 @@
 from fluent_compiler.bundle import FluentBundle
 from fluentogram import FluentTranslator, TranslatorHub
+from typing import Any, Awaitable, Callable, Dict
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject, User
 
+
+class TranslatorRunnerMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: Dict[str, Any]
+    ) -> Any:
+        user: User = data.get('event_from_user')
+        
+        if user is None:
+            return await handler(event, data)
+
+        hub: TranslatorHub = data.get('_translator_hub')
+        data['i18n'] = hub.get_translator_by_locale(locale=user.language_code)
+
+        return await handler(event, data)
 
 def create_translator_hub() -> TranslatorHub:
     translator_hub = TranslatorHub(
